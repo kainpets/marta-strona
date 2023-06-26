dedb325a7f04cf7b6df35aee1954e33b626b7c93
pawel_stepniak <pawelstepniak22@gmail.com>
2023-06-26 21:24:21 +0200

    basic layout styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main>
-      <h1>Jestem Marta</h1>
+      <h1>Pierwszy Taniec Poznań</h1>
+      <h5>ABY PIERWSZY TANIEC BYŁ DLA WAS PRZYGODĄ</h5>
       
     </main>
   )
