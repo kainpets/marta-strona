19d83f04134e3017c265521d582b1d395aa7e2cf
pawel_stepniak <pawelstepniak22@gmail.com>
2023-06-27 23:11:56 +0200

    size text on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       fill={true}
       style={{objectFit: "cover", zIndex: -1}}
       />
-      <div className="flex flex-col max-w-lg">
+      <div className="flex flex-col max-w-sm">
       <h1 className="text-6xl">Pierwszy Taniec Poznań</h1>
       <h5 className="text-3xl">ABY PIERWSZY TANIEC BYŁ DLA WAS PRZYGODĄ</h5>
       </div>
