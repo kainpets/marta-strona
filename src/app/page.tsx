1a24a183896493105b81b55d128e7920ff2b8b15
pawel_stepniak <pawelstepniak22@gmail.com>
2023-07-02 23:51:26 +0200

    uslugi section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
 import MainNav from "@/components/ui/MainNav"
 import Image from "next/image"
+import juliaMilosz from "/public/Julia&Miłosz_672.jpg"
 
 export default function Home() {
   return (
     <main className="overflow-hidden text-white px-16 pt-16 max-w-screen-2xl m-auto">
-      <Image 
-      src="/Julia&Miłosz_672.jpg"
-      alt="Zakochana para"
-      fill={true}
-      priority
-      style={{objectFit: "cover", zIndex: -1, width: "100%", height: "100%"}}
+      <Image
+        src={juliaMilosz}
+        alt="Zakochana para"
+        fill={true}
+        priority
+        quality={100}
+        placeholder="blur"
+        style={{ objectFit: "cover", zIndex: -1, width: "100%", height: "100%" }}
       />
       <MainNav />
       <div className="flex flex-col max-w-sm ">
-      <h1 className="text-6xl leading-relaxed font-bold">Pierwszy Taniec Poznań</h1>
-      <h5 className="text-3xl leading-snug">ABY PIERWSZY TANIEC <br></br> BYŁ DLA WAS PRZYGODĄ</h5>
+        <h1 className="text-6xl leading-relaxed font-bold">Pierwszy Taniec Poznań</h1>
+        <h5 className="text-3xl leading-snug">ABY PIERWSZY TANIEC <br></br> BYŁ DLA WAS PRZYGODĄ</h5>
       </div>
     </main>
   )
--- src/app/uslugi/page.tsx
+++ src/app/uslugi/page.tsx
@@ -5,27 +5,47 @@ import Link from "next/link"
 function Uslugi() {
   return (
     <div className="bg-slate-200 w-screen min-h-screen ">
-    <main className="max-w-screen-2xl m-auto pt-16 px-16 ">
-      <SideNav />
-      <div className="flex flex-row gap-8">
-        <div>
-          <Image src="/martanogadogory.JPG" alt="Marta tańczy z nogą uniesioną do góry" width={200} height={200} />
-          <h2>WARSZTATY TANECZNE</h2>
-          <p>Prowadzę zajęcia taneczne na eventach, warsztaty dla osób początkujących oraz tancerzy. Zapraszam na grupowe zajęcia taneczne dla wszystkich grup zaawansowania. </p>
+      <main className="max-w-screen-2xl m-auto pt-16 px-16">
+        <SideNav />
+        <div style={{
+          display: "grid",
+          gridGap: "34px",
+          gridTemplateColumns: "repeat(auto-fit, minmax(200px, auto))",
+          paddingTop: "2rem",
+          alignItems: "baseline",
+        }}>
+          <div style={{ position: "relative", height: "400px" }}>
+            <Image src="/martanogadogory.JPG" alt="Marta tańczy z nogą uniesioną do góry" width={200} height={200} sizes="33vw" style={{
+              objectFit: "cover",
+              width: "100%",
+              height: "70%"
+            }} />
+            <h2>WARSZTATY TANECZNE</h2>
+            <p className="text-justify">Prowadzę zajęcia taneczne na eventach, warsztaty dla osób początkujących oraz tancerzy. Zapraszam na grupowe zajęcia taneczne dla wszystkich grup zaawansowania. </p>
+          </div>
+          <div style={{ position: "relative", height: "400px" }}>
+            <Image src="/martaradzi.JPG" alt="Para tańczy na pierwszym planie, Marta doradza w tle" width={200} height={200} sizes="33vw" style={{
+              objectFit: "cover",
+              width: "100%",
+              height: "70%"
+            }} />
+            <h2>PIERWSZY TANIEC</h2>
+            <p className="text-justify">Pierwszy taniec to głównie lekcje indywidualne dla par młodych. Zaczynamy od podstawowych kroków tańca użytkowego, potem przechodzimy do przygotowanej dla Was choreografii. </p>
+          </div>
+          <div style={{ position: "relative", height: "400px" }}>
+            <Image src="/martalabedz.JPG" alt="Marta tańczy we wdzięcznej pozie" width={200} height={200} sizes="33vw" style={{
+              objectFit: "cover",
+              width: "100%",
+              height: "70%"
+            }} />
+            <h2>TANIEC DLA KOBIET</h2>
+            <p className="text-justify">Zajęcia pełne kobiecości, na których każda z Was może poczuć się jak gwiazda Broadway&apos;u. Prowadzę zajęcia grupowe i indywidualne. Realizuję też tańce na wieczorach panieńskich.</p>
+          </div>
         </div>
-        <div>
-          <Image src="/martaradzi.JPG" alt="Marta tańczy z nogą uniesioną do góry" width={200} height={200} />
-          <h2>PIERWSZY TANIEC</h2>
-          <p>Pierwszy taniec to głównie lekcje indywidualne dla par młodych. Zaczynamy od podstawowych kroków tańca użytkowego, potem przechodzimy do przygotowanej dla Was choreografii. </p>
+        <div className=" sm:fixed sm:bottom-8 border-8 border-solid border-orange-500 margin-auto w-2/4 ">
+          <h3 >Chcesz dowiedzieć się więcej? <Link href="/kontakt" className="font-bold">Zadzwoń!</Link></h3>
         </div>
-        <div>
-          <Image src="/martalabedz.JPG" alt="Marta tańczy z nogą uniesioną do góry" width={200} height={200} />
-          <h2>TANIEC DLA KOBIET</h2>
-          <p>Zajęcia pełne kobiecości, na których każda z Was może poczuć się jak gwiazda Broadway&apos;u. Prowadzę zajęcia grupowe i indywidualne. Realizuję też tańce na wieczorach panieńskich.</p>
-        </div>
-      </div>
-      <h3 className="text-bold">Chcesz dowiedzieć się więcej? <Link href="/kontakt">Zadzwoń</Link></h3>
-    </main>
+      </main>
     </div>
   )
 }
