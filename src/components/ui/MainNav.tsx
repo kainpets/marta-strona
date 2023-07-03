4aa1c09095ac1eccaf7a0d88a44bf64035907fd7
pawel_stepniak <pawelstepniak22@gmail.com>
2023-07-04 00:44:02 +0200

    icons for navbars

--- src/components/ui/MainNav.tsx
+++ src/components/ui/MainNav.tsx
@@ -1,39 +1,45 @@
 import Link from "next/link"
 import Image from "next/image"
 import logoHomePage from "/public/logoHomePage.png"
+import { Album, ClipboardCheck, PhoneOutgoing, Presentation, User2 } from "lucide-react"
 
 function MainNav() {
   return (
     <div className="flex flex-row justify-between text-white">
-        <Image
-          src={logoHomePage}
-          placeholder="blur"
-          alt="Logo Pierwszy Taniec Poznań"
-          style={{ borderRadius: "50%", flexShrink: "0", maxWidth: "200px"}}
-        />
+      <Image
+        src={logoHomePage}
+        placeholder="blur"
+        alt="Logo Pierwszy Taniec Poznań"
+        style={{ borderRadius: "50%", flexShrink: "0", maxWidth: "200px" }}
+      />
       <nav>
         <li className='flex items-center px-4 justify-between gap-6 text-xl'>
-          <div>
+          <div className="flex flex-row gap-2">
+            <User2 />
             <Link href="/o-mnie">
               O mnie
             </Link>
           </div>
-          <div>
+          <div className="flex flex-row gap-2">
+            <Presentation />
             <Link href="/uslugi">
               Usługi
             </Link>
           </div>
-          <div>
+          <div className="flex flex-row gap-2">
+            <Album />
             <Link href="/portfolio">
               Portfolio
             </Link>
           </div>
-          <div>
+          <div className="flex flex-row gap-2">
+            <ClipboardCheck />
             <Link href="/opinie">
               Opinie
             </Link>
           </div>
-          <div>
+          <div className="flex flex-row gap-2">
+            <PhoneOutgoing />
             <Link href="/kontakt">
               Kontakt
             </Link>
--- src/components/ui/SideNav.tsx
+++ src/components/ui/SideNav.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import logoOtherPages from "/public/logoOtherPages.png"
+import { Album, ClipboardCheck, PhoneOutgoing, Presentation, User2 } from 'lucide-react'
 
 function SideNav() {
   return (
     <div>
       <div className="flex px-4 justify-between text-black max-w-screen-2xl m-auto">
         <Link href="/" passHref>
-          <Image 
+          <Image
             src={logoOtherPages}
             placeholder='blur'
             className='hover:shadow-lg hover:shadow-black/30 transition duration-300 ease-in-out hover:scale-110'
@@ -17,27 +18,32 @@ function SideNav() {
         </Link>
         <nav>
           <li className='flex items-center px-4 justify-between gap-4'>
-            <div>
+            <div className="flex flex-row gap-2">
+              <User2 />
               <Link href="/o-mnie">
                 O mnie
               </Link>
             </div>
-            <div>
+            <div className="flex flex-row gap-2">
+              <Presentation />
               <Link href="/uslugi">
                 Usługi
               </Link>
             </div>
-            <div>
+            <div className="flex flex-row gap-2">
+              <Album />
               <Link href="/portfolio">
                 Portfolio
               </Link>
             </div>
-            <div>
+            <div className="flex flex-row gap-2">
+              <ClipboardCheck />
               <Link href="/opinie">
                 Opinie
               </Link>
             </div>
-            <div>
+            <div className="flex flex-row gap-2">
+              <PhoneOutgoing />
               <Link href="/kontakt">
                 Kontakt
               </Link>
