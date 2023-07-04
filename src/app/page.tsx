9d5ccada96b53a3fa5a046e248b607f470de1006
pawel_stepniak <pawelstepniak22@gmail.com>
2023-07-04 22:09:35 +0200

    landing image optimized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,23 @@ import juliaMilosz from "/public/Julia&Miłosz_672.jpg"
 
 export default function Home() {
   return (
-    <main className="overflow-hidden text-white px-16 pt-16 max-w-screen-2xl m-auto">
+    <main className="relative w-screen h-screen overflow-hidden text-white ">
       <Image
         src={juliaMilosz}
-        alt="Zakochana para"
+        alt="Zakochana para weselna"
         fill={true}
         priority
         quality={100}
+        sizes="100vw"
         placeholder="blur"
-        style={{ objectFit: "cover", zIndex: -1, width: "100%", height: "100%" }}
+        style={{ objectFit: "cover", zIndex: -1, }}
       />
-      <MainNav />
-      <div className="flex flex-col max-w-sm ">
-        <h1 className="text-6xl leading-relaxed font-bold">Pierwszy Taniec Poznań</h1>
-        <h5 className="text-3xl leading-snug">ABY PIERWSZY TANIEC <br></br> BYŁ DLA WAS PRZYGODĄ</h5>
+      <div className="max-w-screen-2xl px-16 pt-16  m-auto">
+        <MainNav />
+        <div className="flex flex-col max-w-sm ">
+          <h1 className="text-6xl leading-relaxed font-bold">Pierwszy Taniec Poznań</h1>
+          <h5 className="text-3xl leading-snug">ABY PIERWSZY TANIEC <br></br> BYŁ DLA WAS PRZYGODĄ</h5>
+        </div>
       </div>
     </main>
   )
