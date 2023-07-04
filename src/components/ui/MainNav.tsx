2d1922e6172c4ee3453bf6475161faa31d222bfb
pawel_stepniak <pawelstepniak22@gmail.com>
2023-07-05 01:10:33 +0200

    mobile navigation

--- src/components/ui/MainNav.tsx
+++ src/components/ui/MainNav.tsx
@@ -1,51 +1,101 @@
+"use client"
+
 import Link from "next/link"
 import Image from "next/image"
 import logoHomePage from "/public/logoHomePage.png"
 import { Album, ClipboardCheck, PhoneOutgoing, Presentation, User2 } from "lucide-react"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 function MainNav() {
   return (
     <div className="flex flex-row justify-between text-white">
       <Image
-      className="w-[100px] md:w-[200px]"
+        className="w-[100px] md:w-[200px]"
         src={logoHomePage}
         placeholder="blur"
         alt="Logo Pierwszy Taniec Poznań"
         style={{ borderRadius: "50%", flexShrink: "0", }}
       />
       <nav>
-        <li className='flex items-center px-4 justify-between gap-6 text-xl'>
-          <div className="hover:bg-slate-600 rounded-lg px-1 transition duration-300">
-            <Link className="flex gap-2" href="/o-mnie">
-            <User2 />
-              O mnie
-            </Link>
-          </div>
-          <div className="hover:bg-slate-600 rounded-lg p-1 transition duration-300">
-            <Link className="flex gap-2" href="/uslugi">
-            <Presentation/>
-              Usługi
-            </Link>
-          </div>
-          <div className="hover:bg-slate-600 rounded-lg p-1 transition duration-300">
-            <Link className="flex gap-2" href="/portfolio">
-            <Album />
-              Portfolio
-            </Link>
-          </div>
-          <div className="hover:bg-slate-600 rounded-lg p-1 transition duration-300">
-            <Link className="flex gap-2" href="/opinie">
-            <ClipboardCheck />
-              Opinie
-            </Link>
-          </div>
-          <div className="hover:bg-slate-600 rounded-lg p-1 transition duration-300">
-            <Link className="flex gap-2" href="/kontakt">
-            <PhoneOutgoing />
-              Kontakt
-            </Link>
-          </div>
-        </li>
+        <div className="md:hidden">
+          <DropdownMenu>
+            <DropdownMenuTrigger>Menu</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+              <Link className="flex gap-2" href="/o-mnie">
+                <User2 />
+                O mnie
+              </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link className="flex gap-2" href="/uslugi">
+                  <Presentation />
+                  Usługi
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+              <Link className="flex gap-2" href="/portfolio">
+                <Album />
+                Portfolio
+              </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+              <Link className="flex gap-2" href="/opinie">
+                <ClipboardCheck />
+                Opinie
+              </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+              <Link className="flex gap-2" href="/kontakt">
+                <PhoneOutgoing />
+                Kontakt
+              </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        <div className="hidden md:block md:w-auto">
+          <li className='flex items-center px-4 justify-between gap-6 text-xl'>
+            <div className="hover:bg-slate-600 rounded-lg px-1 transition duration-300">
+              <Link className="flex gap-2" href="/o-mnie">
+                <User2 />
+                O mnie
+              </Link>
+            </div>
+            <div className="hover:bg-slate-600 rounded-lg p-1 transition duration-300">
+              <Link className="flex gap-2" href="/uslugi">
+                <Presentation />
+                Usługi
+              </Link>
+            </div>
+            <div className="hover:bg-slate-600 rounded-lg p-1 transition duration-300">
+              <Link className="flex gap-2" href="/portfolio">
+                <Album />
+                Portfolio
+              </Link>
+            </div>
+            <div className="hover:bg-slate-600 rounded-lg p-1 transition duration-300">
+              <Link className="flex gap-2" href="/opinie">
+                <ClipboardCheck />
+                Opinie
+              </Link>
+            </div>
+            <div className="hover:bg-slate-600 rounded-lg p-1 transition duration-300">
+              <Link className="flex gap-2" href="/kontakt">
+                <PhoneOutgoing />
+                Kontakt
+              </Link>
+            </div>
+          </li>
+        </div>
       </nav>
     </div>
   )
--- src/components/ui/SideNav.tsx
+++ src/components/ui/SideNav.tsx
@@ -1,7 +1,10 @@
+"use client"
+
 import Link from 'next/link'
 import Image from 'next/image'
 import logoOtherPages from "/public/logoOtherPages.png"
 import { Album, ClipboardCheck, PhoneOutgoing, Presentation, User2 } from 'lucide-react'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from './dropdown-menu'
 
 function SideNav() {
   return (
@@ -17,38 +20,78 @@ function SideNav() {
           />
         </Link>
         <nav>
-          <li className='flex items-center px-4 justify-between gap-4'>
-            <div className="hover:bg-slate-400 rounded-lg p-1 transition duration-300">
-              <Link className="flex gap-2" href="/o-mnie">
-              <User2 />
-                O mnie
-              </Link>
-            </div>
-            <div className="hover:bg-slate-400 rounded-lg p-1 transition duration-300">
-              <Link className="flex gap-2" href="/uslugi">
-              <Presentation />
-                Usługi
-              </Link>
-            </div>
-            <div className="hover:bg-slate-400 rounded-lg p-1 transition duration-300">
-              <Link className="flex gap-2" href="/portfolio">
-              <Album />
-                Portfolio
-              </Link>
-            </div>
-            <div className="hover:bg-slate-400 rounded-lg p-1 transition duration-300">
-              <Link className="flex gap-2" href="/opinie">
-              <ClipboardCheck />
-                Opinie
-              </Link>
-            </div>
-            <div className="hover:bg-slate-400 rounded-lg p-1 transition duration-300">
-              <Link className="flex gap-2" href="/kontakt">
-              <PhoneOutgoing />
-                Kontakt
-              </Link>
-            </div>
-          </li>
+          <div className="md:hidden">
+            <DropdownMenu>
+              <DropdownMenuTrigger>Menu</DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Link className="flex gap-2" href="/o-mnie">
+                    <User2 />
+                    O mnie
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link className="flex gap-2" href="/uslugi">
+                    <Presentation />
+                    Usługi
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link className="flex gap-2" href="/portfolio">
+                    <Album />
+                    Portfolio
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link className="flex gap-2" href="/opinie">
+                    <ClipboardCheck />
+                    Opinie
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link className="flex gap-2" href="/kontakt">
+                    <PhoneOutgoing />
+                    Kontakt
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+          <div className="hidden md:block md:w-auto">
+            <li className='flex items-center px-4 justify-between gap-4'>
+              <div className="hover:bg-slate-400 rounded-lg p-1 transition duration-300">
+                <Link className="flex gap-2" href="/o-mnie">
+                  <User2 />
+                  O mnie
+                </Link>
+              </div>
+              <div className="hover:bg-slate-400 rounded-lg p-1 transition duration-300">
+                <Link className="flex gap-2" href="/uslugi">
+                  <Presentation />
+                  Usługi
+                </Link>
+              </div>
+              <div className="hover:bg-slate-400 rounded-lg p-1 transition duration-300">
+                <Link className="flex gap-2" href="/portfolio">
+                  <Album />
+                  Portfolio
+                </Link>
+              </div>
+              <div className="hover:bg-slate-400 rounded-lg p-1 transition duration-300">
+                <Link className="flex gap-2" href="/opinie">
+                  <ClipboardCheck />
+                  Opinie
+                </Link>
+              </div>
+              <div className="hover:bg-slate-400 rounded-lg p-1 transition duration-300">
+                <Link className="flex gap-2" href="/kontakt">
+                  <PhoneOutgoing />
+                  Kontakt
+                </Link>
+              </div>
+            </li>
+          </div>
         </nav>
       </div>
     </div>
