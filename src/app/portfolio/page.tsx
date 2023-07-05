67bd0abdeb56d3351a2e608fe52063274a0b5173
pawel_stepniak <pawelstepniak22@gmail.com>
2023-07-05 22:11:53 +0200

    font and button portfolio section

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -5,7 +5,7 @@ function Portfolio() {
     <div className="bg-slate-200 w-screen min-h-screen">
       <main className="max-w-screen-2xl m-auto pt-16 px-16">
         <SideNav />
-        <div className="pt-16">
+        <div className="pt-16 text-center">
           <h2>Zapraszam do zapoznania się z występami moich pięknych par młodych, z których jestem BARDZO dumna! Więcej na moim profilu na YouTube.</h2>
           <div style={{
             display: "grid",
@@ -19,9 +19,10 @@ function Portfolio() {
             <iframe className="w-full h-full aspect-video" src="https://www.youtube.com/embed/LqvE-7Xv5fE" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
           </div>
           <div className="w-1/2 mx-auto text-center pt-8 ">
-            <button className="h-8 px-4 bg-green-400 text-white rounded-xl hover:scale-125 hover:bg-green-500 transition duration-500">
-              <a target="_blank" href="https://www.youtube.com/@martaklisowska2017">ZOBACZ WIĘCEJ</a>
-            </button>
+            <a target="_blank" href="https://www.youtube.com/@martaklisowska2017">
+              <button className="h-8 px-4 bg-green-400 text-white shadow rounded-xl hover:scale-125 hover:bg-green-500 transition duration-500">ZOBACZ WIĘCEJ
+              </button>
+            </a>
           </div>
         </div>
       </main>
