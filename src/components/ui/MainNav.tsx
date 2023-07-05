551d390da6ba70e463b56ed6928251c416046807
pawel_stepniak <pawelstepniak22@gmail.com>
2023-07-05 21:04:06 +0200

    padding on nav link element

--- src/components/ui/MainNav.tsx
+++ src/components/ui/MainNav.tsx
@@ -20,7 +20,7 @@ function MainNav() {
         </div>
         <div className="hidden md:block md:w-auto lg:text-xl md:text-sm">
           <li className='flex items-center px-4 justify-between gap-6 '>
-            <div className="hover:bg-slate-600 rounded-lg px-1 transition duration-300">
+            <div className="hover:bg-slate-600 rounded-lg p-1 transition duration-300">
               <Link className="flex gap-2" href="/o-mnie">
                 <User2 />
                 O mnie
