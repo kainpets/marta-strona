933f2b31a3fe4b5f971f8503a8cb957091edd8b3
pawel_stepniak <pawelstepniak22@gmail.com>
2023-07-05 23:13:27 +0200

    not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import SideNav from "@/components/ui/SideNav"
+import { Frown } from "lucide-react"
+
+function NotFound() {
+  return (
+    <main className="max-w-screen-2xl m-auto pt-16 px-16">
+      <SideNav />
+      <div className="pt-16 flex flex-col items-center justify-center">
+        <h1 className="text-3xl" >Nie znaleziono takiej strony</h1>
+        <Frown size={48} />
+      </div>
+    </main>
+  )
+}
+
+export default NotFound
